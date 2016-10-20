a5bdaa5b1f79e6c26200d92144744ce5507892a8
nabil2199 <nabil2199@gmail.com>
2016-10-20 15:22:40 +0200

    excluded common patterns via loop

--- script_prov.ps1
+++ script_prov.ps1
@@ -50,8 +50,12 @@ ForEach ($user in $users)
 
 	}
 
-#Generating Random PIN
+#Generating Random non trivial PIN
     $PIN=Get-Random -Maximum 1000000 -Minimum 100000
+    do{
+        $PIN=Get-random -Maximum 1000000 -Minimum 100000
+    }until($PIN -ne 123456 -And $PIN -ne 234567 -And $PIN -ne 345678 -And $PIN -ne 456789 -And $PIN -ne 111111 -And $PIN -ne 222222 -And $PIN -ne 333333 -And $PIN -ne 444444 -And $PIN -ne 555555 -And $PIN -ne 666666 -And $PIN -ne 777777 -And $PIN -ne 888888 -And $PIN -ne 999999 -And $PIN -ne 000000)
+
 #Setting PIN
 	Set-CsPinSendCAWelcomeMail -UserUri $user.upn -From "weconnect@generali.fr" -Subject "Votre nouveau PIN Lync" -UserEmailAddress $users.EmailAddress -Pin $PIN -Force -SmtpServer rapport.groupe.generali.fr -Credential $mycreds
 	if ($?=$true){
